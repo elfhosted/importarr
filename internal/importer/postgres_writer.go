d9cfea32fe19d3a241de1042f52a52818669a74c
David Young <davidy@funkypenguin.co.nz>
2025-04-14 18:45:09 +1200

    try again
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- internal/importer/postgres_writer.go
+++ internal/importer/postgres_writer.go
@@ -81,17 +81,18 @@ func (pw *PostgresWriter) GetTables() ([]string, error) {
 }
 
 func (pw *PostgresWriter) UpdateSequence(tableName, columnName string) error {
-	query := `
+    // Dynamically construct the query with table and column names
+    query := fmt.Sprintf(`
         SELECT setval(
-            pg_get_serial_sequence($1, $2),
-            COALESCE(MAX($3), 1) + 1
+            pg_get_serial_sequence('"%s"', '%s'),
+            COALESCE(MAX("%s"), 1) + 1
         )
-        FROM $4
-    `
+        FROM "%s"
+    `, tableName, columnName, columnName, tableName)
 
-	_, err := pw.db.Exec(query, tableName, columnName, columnName, tableName)
-	if err != nil {
-		return fmt.Errorf("failed to update sequence for table %s: %w", tableName, err)
-	}
-	return nil
+    _, err := pw.db.Exec(query)
+    if err != nil {
+        return fmt.Errorf("failed to update sequence for table %s: %w", tableName, err)
+    }
+    return nil
 }
